3bdc59e5afde5b00b2776fb0fef5448cede1d052
Visvaldas <visva.rapalis@gmail.com>
2020-12-01 14:48:20 +0200

    Edit add-todo and todo class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { renderAddTodo } from './add-todo.js'
 import { projectDisplayListener } from './pLogic.js'
 import { showProjects } from './show-project.js'
+import { Storage } from './localStorage.js'
 
 const todoApp = (() => {
     renderAddTodo();
