a97a5d4f5c1c6ad6ea04baf707418f55cf4da7db
Guy <guymoustapha@gmail.com>
2020-12-03 17:17:39 +0200

    add  add-todo test

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,6 @@
 const sum = require('./sum');
 import { Project } from './projectDom';
+import { Todo } from './classDom'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -8,6 +9,12 @@ test('adds 1 + 2 to equal 3', () => {
 test('It creates new project', () => {
   const newAddToProject = new Project('Test project title');
   expect(newAddToProject.title).toBe('Test project title');
+});
+
+test('It creates new todo', () => {
+  const newAddTodo = new Todo('title-test', Date.now(), 'high');
+  expect(newAddTodo.title).toBe('title-test');
+  expect(newAddTodo.priority).toBe('high');  
 });
 
 // test('It creates a new To-Do', () => {
